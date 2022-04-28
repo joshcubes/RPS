dba70172335db90b9f0fad908583a6b1d5ae8d5b
joshcubes <76458322+joshcubes@users.noreply.github.com>
2022-04-28 20:58:55 +0100

    Finished Game

--- index.js
+++ index.js
@@ -19,6 +19,59 @@ function Game() {
 
     prompt.get(['choice'], function (err, result) {
         if (err) { return onErr(err); }
-        console.log(`  choice: ${result.choice}`.rainbow);
+        if(!result.choice){
+            console.log('Please enter a valid choice of "Rock", "Paper" or "Scissors".'.red);
+            Game();
+        }else if(result.choice == 'Rock' || result.choice == 'rock'){
+            var userChoice = 'Rock';
+        }else if(result.choice == 'Paper' || result.choice == 'paper'){
+            var userChoice = 'Paper';
+        }else if(result.choice == 'Scissors' || result.choice == 'scissors'){
+            var userChoice = 'Scissors';
+        }else{
+            console.log('Please enter a valid choice of "Rock", "Paper" or "Scissors".'.red);
+            Game();
+            return(0)
+        }
+        var computerChoice = Math.floor(Math.random() * (4 - 1) + 1);
+        if(computerChoice == 1){
+            var computerChoice = 'Rock';
+        }else if(computerChoice == 2){
+            var computerChoice = 'Paper';
+        }else if(computerChoice == 3){
+            var computerChoice = 'Scissors';
+        }
+        if(userChoice == computerChoice){
+            console.log('You both chose ' + userChoice + '! It is a tie!'.green);
+        }else if(userChoice == 'Rock' && computerChoice == 'Paper'){
+            console.log('The computer chose ' + computerChoice + '! Paper Wraps Rock! You lose!'.red);
+        }else if(userChoice == 'Rock' && computerChoice == 'Scissors'){
+            console.log('The computer chose ' + computerChoice + '! Rock Bashes Scissors! You win!'.green);
+        }else if(userChoice == 'Paper' && computerChoice == 'Rock'){
+            console.log('The computer chose ' + computerChoice + '! Paper Wraps Rock! You win!'.green);
+        }else if(userChoice == 'Paper' && computerChoice == 'Scissors'){
+            console.log('The computer chose ' + computerChoice + '! Scissors Cuts Paper! You lose!'.red);
+        }else if(userChoice == 'Scissors' && computerChoice == 'Rock'){
+            console.log('The computer chose ' + computerChoice + '! Rock Bashes Scissors! You lose!'.red);
+        }else if(userChoice == 'Scissors' && computerChoice == 'Paper'){
+            console.log('The computer chose ' + computerChoice + '! Scissors Cuts Paper! You win!'.green);
+        }
+        Again();
     });
 }
+function Again() {
+    console.log('\n\nWould you like to play again? (y/n)'.green);
+    prompt.start();
+    prompt.get(['play'], function (err, result) {
+        if (err) { return onErr(err); }
+        if (result.play == 'y' || result.play == 'Y') {
+            Game();
+        } else if (result.play == 'n' || result.play == 'N') {
+            console.log('Thanks for playing!'.green);
+        } else {
+            console.log('Please enter a valid choice of "y" or "n".'.red);
+            again();
+        }
+    });
+}
+
