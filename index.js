d0b58e578c1a9473bea336b580f0a4da84647341
joshcubes <76458322+joshcubes@users.noreply.github.com>
2022-04-28 21:00:06 +0100

    Remove Unncecary Code

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// console.log('hello'.green); // outputs green text
-// console.log('i like cake and pies'.underline.red) // outputs red underlined text
-// console.log('inverse the color'.inverse); // inverses the color
-// console.log('OMG Rainbows!'.rainbow); // rainbow
-
 var prompt = require('prompt');
 var colors = require('colors');
 
 console.log('Welcome to the Rock Paper Scissors Game! \n To play, enter your choice of rock, paper, or scissors. \n The computer will then also choose an option and then will comapre the two seeing who won! \n\nPlease Enter Your Choice Of "Rock", "Paper" or "Scissors":'.green);
 Game();
-
-onErr = function (err) {
-    console.log(err);
-    return 1;
-}
 
 function Game() {
     prompt.start();
@@ -59,6 +49,7 @@ function Game() {
         Again();
     });
 }
+
 function Again() {
     console.log('\n\nWould you like to play again? (y/n)'.green);
     prompt.start();
@@ -75,3 +66,7 @@ function Again() {
     });
 }
 
+onErr = function (err) {
+    console.log(err);
+    return 1;
+}
